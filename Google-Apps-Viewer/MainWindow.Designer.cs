fb412291bea8b3b80349ec6b3177ed5652fd4a17
Mariusz <mariusz.dolinski@p.lodz.pl>
2023-02-13 16:19:25 +0100

    Added first to filters

--- Google-Apps-Viewer/MainWindow.Designer.cs
+++ Google-Apps-Viewer/MainWindow.Designer.cs
@@ -28,7 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.panel1 = new System.Windows.Forms.Panel();
             this.dataGridView = new System.Windows.Forms.DataGridView();
             this.NameCol = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.CategoryCol = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -36,23 +35,23 @@
             this.ReviewsCol = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.SizeCol = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.TypeCol = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.splitContainer = new System.Windows.Forms.SplitContainer();
+            this.filterPanel = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.labelCategory = new System.Windows.Forms.Label();
+            this.categoryComboBox = new System.Windows.Forms.ComboBox();
+            this.labelName = new System.Windows.Forms.Label();
+            this.textBoxName = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer)).BeginInit();
+            this.splitContainer.Panel1.SuspendLayout();
+            this.splitContainer.Panel2.SuspendLayout();
+            this.splitContainer.SuspendLayout();
+            this.filterPanel.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // panel1
-            // 
-            this.panel1.BackColor = System.Drawing.Color.LightSteelBlue;
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
-            this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(200, 450);
-            this.panel1.TabIndex = 0;
             // 
             // dataGridView
             // 
-            this.dataGridView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
             this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.NameCol,
@@ -61,10 +60,11 @@
             this.ReviewsCol,
             this.SizeCol,
             this.TypeCol});
-            this.dataGridView.Location = new System.Drawing.Point(200, 0);
+            this.dataGridView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView.Location = new System.Drawing.Point(0, 0);
             this.dataGridView.Name = "dataGridView";
             this.dataGridView.RowTemplate.Height = 25;
-            this.dataGridView.Size = new System.Drawing.Size(709, 438);
+            this.dataGridView.Size = new System.Drawing.Size(691, 450);
             this.dataGridView.TabIndex = 1;
             // 
             // NameCol
@@ -102,23 +102,103 @@
             this.TypeCol.HeaderText = "Type";
             this.TypeCol.Name = "TypeCol";
             // 
+            // splitContainer
+            // 
+            this.splitContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer.Name = "splitContainer";
+            // 
+            // splitContainer.Panel1
+            // 
+            this.splitContainer.Panel1.Controls.Add(this.filterPanel);
+            // 
+            // splitContainer.Panel2
+            // 
+            this.splitContainer.Panel2.Controls.Add(this.dataGridView);
+            this.splitContainer.Size = new System.Drawing.Size(996, 450);
+            this.splitContainer.SplitterDistance = 304;
+            this.splitContainer.SplitterWidth = 1;
+            this.splitContainer.TabIndex = 2;
+            // 
+            // filterPanel
+            // 
+            this.filterPanel.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.filterPanel.Controls.Add(this.textBoxName);
+            this.filterPanel.Controls.Add(this.labelName);
+            this.filterPanel.Controls.Add(this.button1);
+            this.filterPanel.Controls.Add(this.labelCategory);
+            this.filterPanel.Controls.Add(this.categoryComboBox);
+            this.filterPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.filterPanel.Location = new System.Drawing.Point(0, 0);
+            this.filterPanel.Name = "filterPanel";
+            this.filterPanel.Size = new System.Drawing.Size(304, 450);
+            this.filterPanel.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(3, 424);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(296, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Apply filters";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.applyFilters_OnClick);
+            // 
+            // labelCategory
+            // 
+            this.labelCategory.AutoSize = true;
+            this.labelCategory.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.labelCategory.Location = new System.Drawing.Point(3, 24);
+            this.labelCategory.Name = "labelCategory";
+            this.labelCategory.Size = new System.Drawing.Size(67, 17);
+            this.labelCategory.TabIndex = 1;
+            this.labelCategory.Text = "Category:";
+            // 
+            // categoryComboBox
+            // 
+            this.categoryComboBox.FormattingEnabled = true;
+            this.categoryComboBox.Location = new System.Drawing.Point(74, 23);
+            this.categoryComboBox.Name = "categoryComboBox";
+            this.categoryComboBox.Size = new System.Drawing.Size(225, 23);
+            this.categoryComboBox.TabIndex = 0;
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.labelName.Location = new System.Drawing.Point(3, 59);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(102, 17);
+            this.labelName.TabIndex = 3;
+            this.labelName.Text = "Name contains:";
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(111, 58);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(188, 23);
+            this.textBoxName.TabIndex = 4;
+            // 
             // MainWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(909, 450);
-            this.Controls.Add(this.dataGridView);
-            this.Controls.Add(this.panel1);
+            this.ClientSize = new System.Drawing.Size(996, 450);
+            this.Controls.Add(this.splitContainer);
             this.Name = "MainWindow";
             this.Text = "Google Apps Viewer";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.splitContainer.Panel1.ResumeLayout(false);
+            this.splitContainer.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer)).EndInit();
+            this.splitContainer.ResumeLayout(false);
+            this.filterPanel.ResumeLayout(false);
+            this.filterPanel.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private Panel panel1;
         private DataGridView dataGridView;
         private DataGridViewTextBoxColumn NameCol;
         private DataGridViewTextBoxColumn CategoryCol;
@@ -126,5 +206,12 @@
         private DataGridViewTextBoxColumn ReviewsCol;
         private DataGridViewTextBoxColumn SizeCol;
         private DataGridViewTextBoxColumn TypeCol;
+        private SplitContainer splitContainer;
+        private Panel filterPanel;
+        private Button button1;
+        private Label labelCategory;
+        private ComboBox categoryComboBox;
+        private Label labelName;
+        private TextBox textBoxName;
     }
 }
--- Google-Apps-Viewer/MainWindow.cs
+++ Google-Apps-Viewer/MainWindow.cs
@@ -6,27 +6,37 @@ namespace Google_Apps_Viewer
 {
     public partial class MainWindow : Form
     {
-        List<GoogleApp> googleApps;
+        #region list of variables
+        private List<GoogleApp> googleApps;
+        public static Dictionary<string, bool> IsFilterActive;
+        #endregion
         public MainWindow()
         {
             InitializeComponent();
-
+            InitializeActiveFilters();
+            //adding categories to comboBox
+            categoryComboBox.Items.Add("");
+            categoryComboBox.Items.AddRange(Enum.GetNames(typeof(Category)));
+            //creating GoogleApp object from csv file
             var currentDir = Directory.GetCurrentDirectory();
             var csvPath = Path.GetRelativePath(currentDir, "GoogleApps/googleplaystore.csv");
             googleApps = LoadGoogleAps(csvPath);
-
-            int i = 0;
-            foreach(var app in googleApps)
-            {
-                dataGridView.Rows.Add(googleApps[i].Name, googleApps[i].Category, 
-                    googleApps[i].Rating, googleApps[i].Reviews, googleApps[i].Size,
-                    googleApps[i].Type);
-                i++;
-                if (i > 100) break;
-            }
-            
         }
 
+        public static void InitializeActiveFilters()
+        {
+            IsFilterActive = new Dictionary<string, bool>()
+            {
+                {"category", false },
+                {"nameContains",false }
+            };
+        }
+
+        /// <summary>
+        /// Convert csv file with google play store apps info, to list of GoogleApp type
+        /// </summary>
+        /// <param name="csvPath"></param>
+        /// <returns></returns>
         static List<GoogleApp> LoadGoogleAps(string csvPath)
         {
             using (var reader = new StreamReader(csvPath))
@@ -37,7 +47,93 @@ namespace Google_Apps_Viewer
                 return records;
             }
         }
+        /// <summary>
+        /// Add one app to dataGridView
+        /// </summary>
+        /// <param name="app">One record from of GoogleApp type</param>
+        private void DisplayApp(GoogleApp app)
+        {
+            dataGridView.Rows.Add(
+                app.Name, 
+                app.Category,
+                app.Rating,
+                app.Reviews,
+                app.Size,
+                app.Type);
+        }
+        /// <summary>
+        /// Display list of apps in dataGridView
+        /// </summary>
+        /// <param name="apps">list of GoogleApps</param>
+        /// <param name="count">number indicated how many apps should be displayed, 
+        /// if 0 whole list is will be displayed</param>
+        private void DisplayApps(List<GoogleApp> apps, int count)
+        {
+            if(count == 0)
+            {
+                foreach (GoogleApp app in apps)
+                {
+                    DisplayApp(app);
+                }
+            }
+            else
+            {
+                int i = 1;
+                foreach (GoogleApp app in apps)
+                {
+                    DisplayApp(app);
+                    i++;
+                    if (i > count)
+                        break;
+                }
+            }           
+        }
 
-        
+        private List<GoogleApp> filterAppsByCategory()
+        {
+            if (categoryComboBox.SelectedItem != null && categoryComboBox.SelectedIndex != 0)
+            {
+                var selectedCategory = categoryComboBox.SelectedItem.ToString();
+                IsFilterActive["category"] = true;
+                return googleApps.Where(a => a.Category.ToString() == selectedCategory).ToList();
+            }
+            IsFilterActive["category"] = false;
+            return new List<GoogleApp>();
+        }
+        private List<GoogleApp> filterByNameContains(string pattern)
+        {
+            if(pattern.Trim() != null && pattern != "")
+            {
+                IsFilterActive["nameContains"] = true;
+                return googleApps.Where(a => a.Name.Contains(pattern)).ToList();
+            }
+            IsFilterActive["nameContains"] = false;
+            return new List<GoogleApp>();       
+        }
+
+        private void applyFilters_OnClick(object sender, EventArgs e)
+        {
+            dataGridView.Rows.Clear();
+            List<GoogleApp> result = googleApps;
+
+            var categories = filterAppsByCategory();    
+            var names = filterByNameContains(textBoxName.Text);
+
+            if (IsFilterActive["category"])
+                result = categories.Intersect(result).ToList();
+            if (IsFilterActive["nameContains"])
+                result = names.Intersect(result).ToList();
+
+            if(result.Count > 1000)
+            {
+                MessageBox.Show("To many results. Showing first 1000.", "To many results",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+                DisplayApps(result, 1000);
+            }
+            else
+            {
+                DisplayApps(result, 0);
+            }
+        }
     }
 }
